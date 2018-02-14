de5013246204352e4120acdcd5ff412bd55b3c70
wataru86 <is151086@cis.aichi-pu.ac.jp>
2018-02-14 10:49:21 +0900

    Add files to preview site in Ciecle ci

---
+++ .circleci/script.hs
@@ -0,0 +1,44 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-10.5 script
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import System.Environment
+import Turtle hiding (header)
+import Data.List
+import qualified Data.Text as T
+import qualified Data.ByteString.Char8 as B
+import Data.Aeson
+import Data.Default.Class
+import Network.HTTP.Req
+
+main :: IO ()
+main = do
+  pullRequestUrl <- lookupEnv "CIRCLE_PULL_REQUEST"
+  maybe (putStrLn "This build is not PR.") main' pullRequestUrl
+
+main' :: String -> IO ()
+main' pullRequestUrl = do
+  putStrLn ("PR: " ++ pullRequestUrl)
+  view $ shell "stack build" empty
+  view $ shell "stack exec site rebuild" empty
+  runReq def $ do
+    userName <- liftIO $ getEnv "CIRCLE_PROJECT_USERNAME"
+    repoName <- liftIO $ getEnv "CIRCLE_PROJECT_REPONAME"
+    buildUrl <- liftIO $ getEnv "CIRCLE_BUILD_URL"
+    homeDir <- liftIO $ getEnv "HOME"
+    let prNumber = reverse . takeWhile (/= '/') $ reverse pullRequestUrl
+    token    <- liftIO $ getEnv "GITHUB_OAUTH"
+    let payload = object
+          [ "body" .= intercalate "/"
+            [buildUrl, "artifacts", "0", homeDir, repoName
+            , "_site/index.html"]
+          ]
+    r <- req POST
+      (https "api.github.com" /: "repos" /: T.pack userName /: T.pack repoName /: "issues" /: T.pack prNumber /: "comments")
+      (ReqBodyJson payload)
+      jsonResponse
+      (oAuth2Token (B.pack token) <>
+      header "User-Agent" "bigmoon")
+    liftIO $ print (responseBody r :: Value)
