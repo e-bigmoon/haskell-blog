9cc774226407946cd8a732b5ab7e2555ecb5c300
waddlaw <ingronze@gmail.com>
2018-02-18 01:18:58 +0900

    Refactoring

--- .circleci/script.hs
+++ .circleci/script.hs
@@ -1,35 +1,47 @@
-#!/usr/bin/env stack
+
 -- stack --resolver lts-10.5 script
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import           Data.Aeson
-import qualified Data.ByteString.Char8 as B
-import           Data.Default.Class
-import           Data.List
-import qualified Data.Text             as T
+import qualified Data.ByteString.Char8      as B (pack)
+import           Data.Default.Class         (def)
+import           Data.List                  (intercalate)
+import           Data.Maybe                 (fromMaybe)
+import qualified Data.Text                  as T (pack)
 import           Network.HTTP.Req
-import           System.Environment
-import           Turtle                hiding (header)
+import           System.Environment         (lookupEnv)
+import           Turtle                     hiding (header)
 
 main :: IO ()
 main = do
+  -- | If this build is part of only one pull request, its URL will be populated here. If there was more than one pull request, it will contain one of the pull request URLs (picked randomly).
   pullRequestUrl <- lookupEnv "CIRCLE_PULL_REQUEST"
   maybe (putStrLn "This build is not PR.") main' pullRequestUrl
 
 main' :: String -> IO ()
-main' pullRequestUrl = do
-  putStrLn ("PR: " ++ pullRequestUrl)
-  view $ shell "stack build" empty
-  view $ shell "stack exec site rebuild" empty
+main' prUrl = do
+  -- | See https://circleci.com/docs/2.0/env-vars/#build-details
+  -- | The username or organization name of the project being tested, i.e. “foo” in circleci.com/gh/foo/bar/123.
+  userName <- fromMaybe "" <$> lookupEnv "CIRCLE_PROJECT_USERNAME"
+  -- | The repository name of the project being tested, i.e. “bar” in circleci.com/gh/foo/bar/123.
+  repoName <- fromMaybe "" <$> lookupEnv "CIRCLE_PROJECT_REPONAME"
+  -- | The URL for the current build.
+  buildUrl <- fromMaybe "" <$> lookupEnv "CIRCLE_BUILD_URL"
+
+  -- | custom environments
+  homeDir  <- fromMaybe "" <$> lookupEnv "HOME"
+  token    <- fromMaybe "" <$> lookupEnv "GITHUB_OAUTH"
+
+  putStrLn ("PR: " ++ prUrl)
+
+  view $ do
+    shell "stack build"             empty
+    shell "stack exec site rebuild" empty
+
   runReq def $ do
-    userName <- liftIO $ getEnv "CIRCLE_PROJECT_USERNAME"
-    repoName <- liftIO $ getEnv "CIRCLE_PROJECT_REPONAME"
-    buildUrl <- liftIO $ getEnv "CIRCLE_BUILD_URL"
-    homeDir  <- liftIO $ getEnv "HOME"
-    let prNumber = reverse . takeWhile (/= '/') $ reverse pullRequestUrl
-    token <- liftIO $ getEnv "GITHUB_OAUTH"
+    let prNumber = takeWhileEnd (/= '/') prUrl
     let payload = object
           [ "body" .= intercalate
               "/"
@@ -41,7 +53,7 @@ main' pullRequestUrl = do
               , "_site/index.html"
               ]
           ]
-    r <- req
+    request <- req
       POST
       (  https "api.github.com"
       /: "repos"
@@ -54,5 +66,13 @@ main' pullRequestUrl = do
       (ReqBodyJson payload)
       jsonResponse
       (oAuth2Token (B.pack token) <> header "User-Agent" "bigmoon")
-    liftIO $ print (responseBody r :: Value)
+    liftIO $ print (responseBody request :: Value)
+
+-- | port from dropWhileEnd in Data.List
+takeWhileEnd :: (a -> Bool) -> [a] -> [a]
+takeWhileEnd p = snd . foldr go (False, [])
+ where
+  go x (isFinish, xs) | not isFinish && p x = (False, x : xs)
+                      | otherwise           = (True, xs)
+
 
