69c35223c1f2e378499a7f2e963b231a47b38af8
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-02-18 14:23:37 +0900

    Refactor いろいろ

--- .circleci/preview.hs
+++ .circleci/preview.hs
@@ -4,69 +4,62 @@
 
 module Main where
 
+import           Control.Exception     (throwIO)
 import           Data.Aeson
-import qualified Data.ByteString.Char8      as B (pack)
-import           Data.Default.Class         (def)
-import           Data.List                  (intercalate)
-import           Data.Maybe                 (fromMaybe)
-import qualified Data.Text                  as T (pack)
+import qualified Data.ByteString.Char8 as B (pack)
+import           Data.Default.Class    (def)
+import           Data.Function         ((&))
+import           Data.List             (intercalate)
+import           Data.Maybe            (fromMaybe)
+import qualified Data.Text             as T (pack)
 import           Network.HTTP.Req
-import           System.Environment         (lookupEnv)
-import           Turtle                     hiding (header)
+import           System.Environment    (lookupEnv)
+import           Turtle                hiding (header)
+
 
 main :: IO ()
 main = do
   -- | If this build is part of only one pull request, its URL will be populated here. If there was more than one pull request, it will contain one of the pull request URLs (picked randomly).
   pullRequestUrl <- lookupEnv "CIRCLE_PULL_REQUEST"
-  maybe (putStrLn "This build is not PR.") main' pullRequestUrl
+  flip (maybe $ putStrLn "This build is not PR.") pullRequestUrl $ \prUrl -> do
+    putStrLn ("PR: " ++ prUrl)
 
-main' :: String -> IO ()
-main' prUrl = do
-  -- | See https://circleci.com/docs/2.0/env-vars/#build-details
-  -- | The username or organization name of the project being tested, i.e. “foo” in circleci.com/gh/foo/bar/123.
-  userName <- fromMaybe "" <$> lookupEnv "CIRCLE_PROJECT_USERNAME"
-  -- | The repository name of the project being tested, i.e. “bar” in circleci.com/gh/foo/bar/123.
-  repoName <- fromMaybe "" <$> lookupEnv "CIRCLE_PROJECT_REPONAME"
-  -- | The URL for the current build.
-  buildUrl <- fromMaybe "" <$> lookupEnv "CIRCLE_BUILD_URL"
+    -- | See https://circleci.com/docs/2.0/env-vars/#build-details
+    -- | The username or organization name of the project being tested, i.e. “foo” in circleci.com/gh/foo/bar/123.
+    userName <- lookupEnvWithError "CIRCLE_PROJECT_USERNAME"
+    -- | The repository name of the project being tested, i.e. “bar” in circleci.com/gh/foo/bar/123.
+    repoName <- lookupEnvWithError "CIRCLE_PROJECT_REPONAME"
+    -- | The URL for the current build.
+    buildUrl <- lookupEnvWithError "CIRCLE_BUILD_URL"
 
-  -- | custom environments
-  homeDir  <- fromMaybe "" <$> lookupEnv "HOME"
-  token    <- fromMaybe "" <$> lookupEnv "GITHUB_OAUTH"
+    -- | custom environments
+    homeDir  <- lookupEnvWithError "HOME"
+    token    <- lookupEnvWithError "GITHUB_OAUTH"
 
-  putStrLn ("PR: " ++ prUrl)
+    view $ do
+      shell "stack build"             empty
+      shell "stack exec site rebuild" empty
 
-  view $ do
-    shell "stack build"             empty
-    shell "stack exec site rebuild" empty
+    let
+      prNumber = takeWhileEnd (/= '/') prUrl
+      url = mkUrl (T.pack userName) (T.pack repoName) (T.pack prNumber)
+      message = mkMessage buildUrl homeDir repoName
 
-  runReq def $ do
-    let prNumber = takeWhileEnd (/= '/') prUrl
-    let payload = object
-          [ "body" .= intercalate
-              "/"
-              [ buildUrl
-              , "artifacts"
-              , "0"
-              , homeDir
-              , repoName
-              , "_site/index.html"
-              ]
-          ]
-    request <- req
-      POST
-      (  https "api.github.com"
-      /: "repos"
-      /: T.pack userName
-      /: T.pack repoName
-      /: "issues"
-      /: T.pack prNumber
-      /: "comments"
-      )
-      (ReqBodyJson payload)
-      jsonResponse
-      (oAuth2Token (B.pack token) <> header "User-Agent" "bigmoon")
-    liftIO $ print (responseBody request :: Value)
+    resp <- runReq def $ mkReq url message (B.pack token)
+    print $ responseBody resp
+
+mkReq :: Url Https -> String -> ByteString -> Req (JsonResponse Value)
+mkReq url message token =
+  req POST url (ReqBodyJson $ object [ "body" .= message]) jsonResponse $
+    oAuth2Token token <> header "User-Agent" "bigmoon"
+
+mkMessage :: String -> String -> String -> String
+mkMessgae buildUrl homeDir repoName =
+  intercalate "/" [buildUrl, "artifacts", "0", homeDir, repoName, "_site/index.html"]
+
+mkUrl :: Text -> Text -> Text -> Url Https
+mkUrl userName repoName prNumber =
+  pure $ https "api.github.com" /: "repos" /: userName /: repoName /: "issues" /: prNumber /: "comments"
 
 -- | port from dropWhileEnd in Data.List
 takeWhileEnd :: (a -> Bool) -> [a] -> [a]
@@ -75,4 +68,6 @@ takeWhileEnd p = snd . foldr go (False, [])
   go x (isFinish, xs) | not isFinish && p x = (False, x : xs)
                       | otherwise           = (True, xs)
 
-
+lookupEnvWithError :: String -> IO String
+lookupEnvWithError env =
+  fromMaybe (throwIO $ env ++ " is not found") <$> lookupEnv env
