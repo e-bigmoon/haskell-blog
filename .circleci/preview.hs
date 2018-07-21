2b8dea87973b8484c0282367697c2f339a89fe32
waddlaw <ingronze@gmail.com>
2018-07-21 13:54:36 +0900

    simplify

--- .circleci/preview.hs
+++ .circleci/preview.hs
@@ -51,7 +51,7 @@ mkReq url message token =
 mkMessage :: String -> String -> String -> String
 mkMessage buildUrl homeDir _repoName = intercalate
   "/"
-  [buildUrl, "artifacts", "0", homeDir, "project/.circleci/code/_site/index.html"]
+  [buildUrl, "artifacts", "0", homeDir, "project/.circleci/_site/index.html"]
 
 mkUrl :: Text -> Text -> Text -> Url Https
 mkUrl userName repoName prNumber =
