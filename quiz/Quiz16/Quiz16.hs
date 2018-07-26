ebf3aef8ba135dc26a22d4cae08e609087fa7942
waddlaw <ingronze@gmail.com>
2018-07-26 18:31:48 +0900

    stylish-haskell 適用

--- quiz/Quiz16/Quiz16.hs
+++ quiz/Quiz16/Quiz16.hs
@@ -20,17 +20,17 @@ intTree =
 
 -- | 部分木を左右反転させた木を返す
 mirror :: Tree a -> Tree a
-mirror (Leaf x) = Leaf x
+mirror (Leaf x)   = Leaf x
 mirror (Node l r) = Node r l
 
 -- | 葉の数を返す
 size :: Tree a -> Int
-size (Leaf _) = 1
+size (Leaf _)   = 1
 size (Node l r) = size l + size r
 
 -- | 木の高さを返す
 depth :: Tree a -> Int
-depth (Leaf _) = 0
+depth (Leaf _)   = 0
 depth (Node l r) = 1 + max (depth l) (depth r)
 
 -- | 木が平衡かどうか
@@ -42,27 +42,27 @@ isBalanced (Node l r) = diff <= 1 && isBalanced l && isBalanced r
 
 -- | 葉の値を集めてリストにして返す
 leaves :: Tree a -> [a]
-leaves (Leaf x) = [x]
+leaves (Leaf x)   = [x]
 leaves (Node l r) = leaves l ++ leaves r
 
 -- | 左の部分木を返す
 left :: Tree a -> Maybe (Tree a)
-left (Leaf _) = Nothing
+left (Leaf _)   = Nothing
 left (Node l _) = Just l
 
 -- | 右の部分木を返す
 right :: Tree a -> Maybe (Tree a)
-right (Leaf _) = Nothing
+right (Leaf _)   = Nothing
 right (Node _ r) = Just r
 
 -- | 木の要素に対して関数を適用する
 mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree f (Leaf x) = Leaf (f x)
+mapTree f (Leaf x)   = Leaf (f x)
 mapTree f (Node l r) = Node (mapTree f l) (mapTree f r)
 
 -- | 木を畳み込む
 foldTree :: (a -> b) -> (b -> b -> b) -> Tree a -> b
-foldTree f _ (Leaf x) = f x
+foldTree f _ (Leaf x)   = f x
 foldTree f g (Node l r) = g (foldTree f g l) (foldTree f g r)
 
 main :: IO ()
