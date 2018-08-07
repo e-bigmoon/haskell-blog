93c10752e37878bcda4e9775bcc968439d3ff9f3
waddlaw <ingronze@gmail.com>
2018-08-07 16:36:54 +0900

    Add article

---
+++ sample-code/2018/08-07/Sample.hs
@@ -0,0 +1,16 @@
+import Data.Char (toUpper)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Control.Arrow
+
+toAllUpper :: String -> String
+toAllUpper = map toUpper
+
+toAllUpperText :: Text -> Text
+toAllUpperText = T.pack . toAllUpper . T.unpack
+
+toAllUpperText' :: Text -> Text
+toAllUpperText' = T.unpack ^>> toAllUpper >>^ T.pack
+
+toAllUpperText'' :: Text -> Text
+toAllUpperText'' = T.pack <<^ toAllUpper ^<< T.unpack
