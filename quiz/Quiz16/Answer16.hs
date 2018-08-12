28fcef683f09bb60aedc3f2c2c5cb90b1ec4fa14
waddlaw <ingronze@gmail.com>
2018-08-12 18:36:35 +0900

    完成

--- quiz/Quiz16/Answer16.hs
+++ quiz/Quiz16/Answer16.hs
@@ -3,6 +3,21 @@
 
 {-# LANGUAGE InstanceSigs #-}
 
+import Test.QuickCheck
+import Test.QuickCheck.Gen
+import Control.Monad
+import Data.Maybe
+
+instance Arbitrary a => Arbitrary (Tree a) where
+  arbitrary = do
+    v <- choose (0 :: Int, 1)
+    if v == 0
+      then Leaf <$> arbitrary
+      else Node <$> arbitrary <*> arbitrary
+  shrink (Leaf _) = []
+  shrink (Node l r) =
+    [l, r] ++ [ Node l' r' | (l', r') <- shrink (l, r) ]
+
 data Tree a
   = Leaf a
   | Node (Tree a) (Tree a)
@@ -31,13 +46,14 @@ depth (Leaf _)   = 0
 depth (Node l r) = 1 + max (depth l) (depth r)
 
 -- | 木が平衡かどうか
--- isBalanced :: Tree a -> Bool
--- isBalanced (Leaf _) = True
--- isBalanced (Node l r) = diff <= 1 && isBalanced l && isBalanced r
---   where
---     diff = abs (size l - size r)
 isBalanced :: Tree a -> Bool
-isBalanced = check . until (and . fmap isLeaf) dig . pure
+isBalanced (Leaf _) = True
+isBalanced (Node l r) = diff <= 1 && isBalanced l && isBalanced r
+  where
+    diff = abs (depth l - depth r)
+
+isBalanced' :: Tree a -> Bool
+isBalanced' = check . until (or . fmap isLeaf) dig . pure
   where
     dig :: [Tree a] -> [Tree a]
     dig = concatMap step
@@ -59,6 +75,10 @@ instance Functor Tree where
   fmap f (Node l r) = Node (fmap f l) (fmap f r)
 
 instance Foldable Tree where
+  foldMap :: Monoid m => (a -> m) -> Tree a -> m
+  foldMap f (Leaf x) = f x
+  foldMap f (Node l r) = foldMap f l `mappend` foldMap f r
+
   foldr :: (a -> b -> b) -> b -> Tree a -> b
   foldr f e (Leaf x)   = f x e
   foldr f e (Node l r) = foldr f (foldr f e r) l
