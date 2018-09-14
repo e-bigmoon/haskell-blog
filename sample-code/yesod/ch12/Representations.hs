bec2eb66c9ed86045b0486c3ccd62276a10dcf5d
HirokiAsano <hiroki.asano.bigmoon@gmail.com>
2018-09-14 16:22:15 +0900

    ch12のサンプルコード追加

---
+++ sample-code/yesod/ch12/Representations.hs
@@ -0,0 +1,34 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-11.7
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+import           Data.Text (Text)
+import           Yesod
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod App
+
+getHomeR :: Handler TypedContent
+getHomeR = selectRep $ do
+    provideRep $ return
+        [shamlet|
+            <p>Hello, my name is #{name} and I am #{age} years old.
+        |]
+    provideRep $ return $ object
+        [ "name" .= name
+        , "age" .= age
+        ]
+  where
+    name = "Michael" :: Text
+    age = 28 :: Int
+
+main :: IO ()
+main = warp 3000 App
