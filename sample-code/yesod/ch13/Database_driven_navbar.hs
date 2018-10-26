34ee6b111e3ff9f7c2f7860f59528e8ca3cb46f0
HirokiAsano <hiroki.asano.bigmoon@gmail.com>
2018-10-26 18:28:10 +0900

    commit source code of ch13

---
+++ sample-code/yesod/ch13/Database_driven_navbar.hs
@@ -0,0 +1,79 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-12.14
+
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+import           Control.Monad.Logger    (runNoLoggingT)
+import           Data.Text               (Text)
+import           Data.Time
+import           Database.Persist.Sqlite
+import           Yesod
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Link
+    title Text
+    url Text
+    added UTCTime
+|]
+
+data App = App ConnectionPool
+
+mkYesod "App" [parseRoutes|
+/         HomeR    GET
+/add-link AddLinkR POST
+|]
+
+instance Yesod App
+
+instance RenderMessage App FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+instance YesodPersist App where
+    type YesodPersistBackend App = SqlBackend
+    runDB db = do
+        App pool <- getYesod
+        runSqlPool db pool
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout
+    [whamlet|
+        <form method=post action=@{AddLinkR}>
+            <p>
+                Add a new link to
+                <input type=url name=url value=http://>
+                titled
+                <input type=text name=title>
+                <input type=submit value="Add link">
+        <h2>Existing links
+        ^{existingLinks}
+    |]
+
+existingLinks :: Widget
+existingLinks = do
+    links <- handlerToWidget $ runDB $ selectList [] [LimitTo 5, Desc LinkAdded]
+    [whamlet|
+        <ul>
+            $forall Entity _ link <- links
+                <li>
+                    <a href=#{linkUrl link}>#{linkTitle link}
+    |]
+
+postAddLinkR :: Handler ()
+postAddLinkR = do
+    url <- runInputPost $ ireq urlField "url"
+    title <- runInputPost $ ireq textField "title"
+    now <- liftIO getCurrentTime
+    runDB $ insert $ Link title url now
+    setMessage "Link added"
+    redirect HomeR
+
+main :: IO ()
+main = runNoLoggingT $ withSqlitePool "links.db3" 10 $ \pool -> liftIO $ do
+    runSqlPersistMPool (runMigration migrateAll) pool
+    warp 3000 $ App pool
---
+++ sample-code/yesod/ch13/adding-a-new-monad-transformer2/Adding-a-new-monad-transformer2.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+import           Control.Monad              (join)
+import           Control.Monad.Catch        (throwM)
+import           UnliftIO.Exception         (catch)
+import           Control.Monad.CryptoRandom
+import           Control.Monad.Error.Class  (MonadError (..))
+import           Crypto.Random              (SystemRandom, newGenIO)
+import           Data.ByteString.Base16     (encode)
+import           Data.IORef
+import           Data.Text.Encoding         (decodeUtf8)
+import           Yesod 
+
+data App = App
+    { randGen :: IORef SystemRandom
+    }
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod App
+
+getHomeR :: Handler Html
+getHomeR = do
+    randomBS <- getBytes 16
+    defaultLayout
+        [whamlet|
+            <p>Here's some random data: #{decodeUtf8 $ encode randomBS}
+        |]
+
+instance MonadError GenError Handler where
+    throwError = throwM
+    catchError = catch
+instance MonadCRandom GenError Handler where
+    getCRandom  = wrap crandom
+    {-# INLINE getCRandom #-}
+    getBytes i = wrap (genBytes i)
+    {-# INLINE getBytes #-}
+    getBytesWithEntropy i e = wrap (genBytesWithEntropy i e)
+    {-# INLINE getBytesWithEntropy #-}
+    doReseed bs = do
+        genRef <- fmap randGen getYesod
+        join $ liftIO $ atomicModifyIORef genRef $ \gen ->
+            case reseed bs gen of
+                Left e -> (gen, throwM e)
+                Right gen' -> (gen', return ())
+    {-# INLINE doReseed #-}
+
+wrap :: (SystemRandom -> Either GenError (a, SystemRandom)) -> Handler a
+wrap f = do
+    genRef <- fmap randGen getYesod
+    join $ liftIO $ atomicModifyIORef genRef $ \gen ->
+        case f gen of
+            Left e -> (gen, throwM e)
+            Right (x, gen') -> (gen', return x)
+
+main :: IO ()
+main = do
+    gen <- newGenIO
+    genRef <- newIORef gen
+    warp 3000 App
+        { randGen = genRef
+        }
