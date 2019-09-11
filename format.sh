e0bc9038c71a0b47015c6e55b436da1f01535624
waddlaw <ingronze@gmail.com>
2019-09-11 13:20:54 +0900

    update script

--- format.sh
+++ format.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-while IFS= read -r -d '' file
-do
-  ormolu "$file" --mode inplace
-done < <(find app/ -type f -name "*hs" -not -path '.git' -not -path '*.dist-newstyle' -not -path '*.stack-work*' -print0)
+ormolu --mode inplace $(find app -type f -name "*.hs")
