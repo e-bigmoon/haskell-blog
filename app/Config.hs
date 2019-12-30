15753636fc422664000b45ef78562962c2c86230
waddlaw <ingronze@gmail.com>
2019-12-30 14:56:02 +0900

    Ormolu 適用

--- app/Config.hs
+++ app/Config.hs
@@ -9,43 +9,43 @@ import Data.Yaml (FromJSON, ParseException, decodeFileEither)
 fromConfig :: (FromJSON a) => FilePath -> IO (Either ParseException a)
 fromConfig = decodeFileEither
 
-type Style
-  = Record
-      [ "headerColour" >: Colour,
-        "headThemeColour" >: HexColour,
-        "footerColour" >: Colour,
-        "footerBtnColour" >: Colour,
-        "footerLinkColour" >: Colour,
-        "navbarTextColourDesktop" >: Colour,
-        "navbarTextColourMobile" >: Colour,
-        "shareButtonColour" >: Colour,
-        "shareButtonSmallColour" >: Colour
-        ]
+type Style =
+  Record
+    [ "headerColour" >: Colour,
+      "headThemeColour" >: HexColour,
+      "footerColour" >: Colour,
+      "footerBtnColour" >: Colour,
+      "footerLinkColour" >: Colour,
+      "navbarTextColourDesktop" >: Colour,
+      "navbarTextColourMobile" >: Colour,
+      "shareButtonColour" >: Colour,
+      "shareButtonSmallColour" >: Colour
+    ]
 
 type Colour = String
 
 type HexColour = String
 
-type General
-  = Record
-      [ "baseUrl" >: String,
-        "headTitle" >: String,
-        "siteTitle" >: String,
-        "paginate" >: Int
-        ]
+type General =
+  Record
+    [ "baseUrl" >: String,
+      "headTitle" >: String,
+      "siteTitle" >: String,
+      "paginate" >: Int
+    ]
 
-type Feed
-  = Record
-      [ "title" >: String,
-        "description" >: String,
-        "authorName" >: String,
-        "authorEmail" >: String,
-        "root" >: String
-        ]
+type Feed =
+  Record
+    [ "title" >: String,
+      "description" >: String,
+      "authorName" >: String,
+      "authorEmail" >: String,
+      "root" >: String
+    ]
 
-type Site
-  = Record
-      [ "general" >: General,
-        "feed" >: Feed,
-        "style" >: Style
-        ]
+type Site =
+  Record
+    [ "general" >: General,
+      "feed" >: Feed,
+      "style" >: Style
+    ]
